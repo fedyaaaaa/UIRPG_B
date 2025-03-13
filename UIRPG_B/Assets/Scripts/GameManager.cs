08a82279aed1d84e6392a17c2824fc131bb5c55a
fedyaaaaa <loreta.razalinska@gmail.com>
2025-03-13 13:50:01 +0200

    Basic UI
    script for UI, button for do round, changing stats upon hitting the button

--- UIRPG_B/Assets/Scripts/GameManager.cs
+++ UIRPG_B/Assets/Scripts/GameManager.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     public Player player;
     public Enemy enemy;
+    [SerializeField] private TMP_Text playerName, playerHealth, enemyName, enemyStats;
     
     
     void Start()
+    {
+        playerName.text = player.CharName;
+        enemyName.text = enemy.name;
+    }
+
+    private void UpdateHealth()
+    {
+        playerHealth.text = player.health.ToString();
+        enemyStats.text = enemy.health.ToString();
+    }
+
+    public void DoRound()
     {
         int damage = player.Attack();
         enemy.GetHit(damage);
@@ -16,7 +31,7 @@ public class GameManager : MonoBehaviour
         int enemyDamage = enemy.Attack();
         player.GetHit(enemyDamage);
         enemy.Weapon.ApplyEffect(player);
+        UpdateHealth();
     }
-
     
 }
